5ae8a6a335745dd59b0d195e6b2d39417827f373
s1ddok <siddok@gmail.com>
2017-04-09 18:44:21 +0200

    add location demo

---
+++ app/src/main/java/pl/pollub/androidprogrammingfundamentals/Lab04Location.java
@@ -0,0 +1,13 @@
+package pl.pollub.androidprogrammingfundamentals;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Lab04Location extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab04_location);
+    }
+}
