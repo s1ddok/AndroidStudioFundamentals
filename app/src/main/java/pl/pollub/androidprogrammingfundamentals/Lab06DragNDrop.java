8fb21a5a4f1f5e36331d75a89f3f8da1ec8530ff
s1ddok <siddok@gmail.com>
2017-04-10 10:07:15 +0200

    add drag-n-drop sample

---
+++ app/src/main/java/pl/pollub/androidprogrammingfundamentals/Lab06DragNDrop.java
@@ -0,0 +1,13 @@
+package pl.pollub.androidprogrammingfundamentals;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Lab06DragNDrop extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab06_drag_ndrop);
+    }
+}
